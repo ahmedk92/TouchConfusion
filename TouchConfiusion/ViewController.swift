17de52b59afaf878f0acab32e77607878fde55d7
Ahmed Khalaf <ahmedkhalaf.92@gmail.com>
2020-06-20 12:05:59 +0200

    Initial Commit

---
+++ TouchConfiusion/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TouchConfiusion
+//
+//  Created by Ahmed Khalaf on 6/20/20.
+//  Copyright © 2020 io.github.ahmedkhalaf. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TouchConfiusionTests/TouchConfiusionTests.swift
@@ -0,0 +1,34 @@
+//
+//  TouchConfiusionTests.swift
+//  TouchConfiusionTests
+//
+//  Created by Ahmed Khalaf on 6/20/20.
+//  Copyright © 2020 io.github.ahmedkhalaf. All rights reserved.
+//
+
+import XCTest
+@testable import TouchConfiusion
+
+class TouchConfiusionTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
